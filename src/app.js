7ab66949b6008a3ca0f5f354824897218251d2c5
Kevin Johnson <kevinj902302@gmail.com>
2021-03-25 21:43:59 -0600

    removes ssl

--- src/app.js
+++ src/app.js
@@ -28,13 +28,14 @@ app.use((req, res, next) => {
 })
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = API_TOKEN
+  const apiToken = process.env.API_TOKEN
   const authToken = req.header('Authorization')
 
   console.log('authtoken========', authToken, 'APITOKEN======', apiToken)
-  if (!authToken || authToken == apiToken) {
+  /**if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
-  }
+  }**/
+  console.log('Done')
   next()
 })
 
