1aeeafb609ee13a7fa7d3f7090ca39f170c4affc
Kevin Johnson <kevinj902302@gmail.com>
2021-03-25 21:31:37 -0600

    Changes apitoken

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(function validateBearerToken(req, res, next) {
   const authToken = req.header('Authorization')
 
   console.log('authtoken========', authToken, 'APITOKEN======', apiToken)
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+  if (!authToken || authToken == apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
   next()
