a2d581c567ae90a153ed548bbe5a923677e9c53e
Kevin Johnson <kevinj902302@gmail.com>
2021-03-25 21:47:56 -0600

    uncomments code

--- src/app.js
+++ src/app.js
@@ -32,10 +32,10 @@ app.use(function validateBearerToken(req, res, next) {
   const authToken = req.header('Authorization')
 
   console.log('authtoken========', authToken, 'APITOKEN======', apiToken)
-  /**if (!authToken || authToken.split(" ")[1] !== apiToken) {
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
-  }**/
-  console.log('Done')
+  }
+  console.log('')
   next()
 })
 
